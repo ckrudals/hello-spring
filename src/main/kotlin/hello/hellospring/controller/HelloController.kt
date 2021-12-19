dc508b7ce5afe428a16ea50f44dacf039d508939
ckrudals <2004ckck@naver.com>
2021-12-19 20:41:13 +0900

    add get Mapping function

--- src/main/kotlin/hello/hellospring/controller/HelloController.kt
+++ src/main/kotlin/hello/hellospring/controller/HelloController.kt
@@ -3,6 +3,8 @@ package hello.hellospring.controller
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
 class HelloController {
@@ -13,8 +15,20 @@ class HelloController {
     // mvc model의 hello
     fun hello(model: Model): String {
         // 데이터를 담는 함수
-        model.addAttribute("data","hello")
+        model.addAttribute("data", "hello")
         return "hello"
 
+    }
+
+    @GetMapping("hello-mvc")
+    fun helloMvc(@RequestParam("name") name: String, model: Model): String {
+        model.addAttribute("name", name)
+        return "hello-template"
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    fun helloString(@RequestParam("name") name: String): String {
+        return "hello $name"
     }
 }
